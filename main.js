3f54f20a247ccf8bde035546ce62d432e7f66330
Trusimo <simonetrunfio96@gmail.com>
2022-05-30 16:20:16 +0200

    gameplay beta

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// Il computer deve generare 16 numeri casuali compresi nel range della griglia: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l’utente clicca su una cella:
+// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba -
+// la cella si colora di rosso e la partita termina,
+// altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio,
+// cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
 const gridContainer = document.querySelector(".grid-container");
 
 // come a lezione bisogna creare una griglia, stavolta di 10x10, usando un ciclo.
@@ -13,8 +23,20 @@ function createGrid(CelleX, CelleY) {
     celle.classList.add("celle");
     celle.textContent = i;
 
+    var bombe = [];
+    for (let n=1, j=16; n<=j; n++) {
+    let numerobomba = Math.round(Math.random() * 100);
+        if (!bombe.includes(numerobomba)) {
+            bombe.push(numerobomba);
+    }
+} console.log(bombe);
+
     celle.addEventListener("click", function(){ 
+        if (bombe.includes(i)) {
+            celle.style.backgroundColor = 'red';
+        } else {
         celle.style.backgroundColor = "lightblue";
+    }
     }
     );
 
